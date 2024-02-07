86046b3f1fadb9322adb836f9e0a20b08231c1e9
Mark Dunne <Markdunne117@gmail.com>
2024-02-07 13:20:15 +0000

    Correct plural category name and configure product admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, ProductCategory
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    # Tuple sorts the products by sku
+    ordering = ('sku',)
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
