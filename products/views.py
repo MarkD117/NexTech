0377b44968c17e5018613aad895c6b7ff9abcf54
Mark Dunne <Markdunne117@gmail.com>
2024-02-08 15:02:18 +0000

    Create product template and add product images

--- products/views.py
+++ products/views.py
@@ -5,10 +5,10 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Products.objects.all()
+    products = Product.objects.all()
 
     context = {
         'products': products,
     }
 
-    return render(request, 'products/products.html')
+    return render(request, 'products/products.html', context)
