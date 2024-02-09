a2b6aec9fbe239cb4de61c3a05514a2afbc13998
Mark Dunne <Markdunne117@gmail.com>
2024-02-09 15:28:33 +0000

    Add urls, view and template for cart

---
+++ cart/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.view_cart, name='view_cart'),
+]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_cart(request):
+    """ This view renders the cart contents page """
+
+    return render(request, 'cart/cart.html')
