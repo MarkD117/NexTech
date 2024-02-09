8ba04e53b45a723cf93f802a5f4f3dfb13830cc9
Mark Dunne <Markdunne117@gmail.com>
2024-02-09 16:54:47 +0000

    Create cart content context file and add to settings

--- nextech/settings.py
+++ nextech/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
                 'django.template.context_processors.media',
             ],
         },
@@ -165,3 +166,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Variables used to calculate delivery costs
+FREE_DELIVERY_THRESHOLD = 150
+STANDARD_DELIVERY_PERCENTAGE = 10
