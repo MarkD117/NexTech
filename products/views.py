67dc495cfdd07bd691eac17f634358a7111c5e9c
Mark Dunne <Markdunne117@gmail.com>
2024-02-15 19:16:45 +0000

    Add login required decorator to product views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -73,6 +74,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ This view handles adding a product to the store """
     # Prevents standard users from accessing view
@@ -99,6 +101,7 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ This view handles editing existing products in the store """
     if not request.user.is_superuser:
@@ -128,6 +131,7 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ This view handles deleting products from the store """
     if not request.user.is_superuser:
