79e123452544192dab9752018a4f8d6f14f746a9
Mark Dunne <Markdunne117@gmail.com>
2024-02-16 22:39:11 +0000

    Remove https from heroku hostname

--- nextech/settings.py
+++ nextech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-markd117-nextech-jaoebjczu8z.ws-eu108.gitpod.io', 'https://nextech-5db9177526a4.herokuapp.com']
+ALLOWED_HOSTS = ['8000-markd117-nextech-jaoebjczu8z.ws-eu108.gitpod.io', 'nextech-5db9177526a4.herokuapp.com']
 
 
 # Application definition
