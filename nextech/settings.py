a9def939d0aa5b9f14703d31bb1baa925acf9a2d
Mark Dunne <Markdunne117@gmail.com>
2024-02-16 22:48:00 +0000

    Set DEBUG to DEVELOPMENT

--- nextech/settings.py
+++ nextech/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-markd117-nextech-jaoebjczu8z.ws-eu108.gitpod.io', 'nextech-5db9177526a4.herokuapp.com']
 
