af554d5a0be10999d14ee0c2c4c8d75fc0c8dbdb
Mark Dunne <Markdunne117@gmail.com>
2024-02-18 22:48:33 +0000

    Add contact form to forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import NewsletterSubscriber
+from .models import NewsletterSubscriber, Contact
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,15 @@ class NewsletterForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['email'].label = False
         self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Your email'})
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'contact-form-input'
