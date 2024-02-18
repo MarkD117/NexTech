e3ca9a6503d85a6e37173fcf689056c01e2a0556
Mark Dunne <Markdunne117@gmail.com>
2024-02-18 22:50:03 +0000

    Register contact form in the admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import NewsletterSubscriber
+from .models import NewsletterSubscriber, Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject')
+    search_fields = ('name', 'email', 'subject')
+    list_filter = ('name', 'email', 'subject')
+
 
 admin.site.register(NewsletterSubscriber)
+admin.site.register(Contact, ContactAdmin)
