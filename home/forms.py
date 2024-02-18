32034741f196f887ae8ee6404ef85ce54d6d9baf
Mark Dunne <Markdunne117@gmail.com>
2024-02-18 17:48:22 +0000

    Remove newsletter from home app

--- home/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Subscriber
-
-class NesletterForm(forms.ModelForm):
-    class Meta:
-        model = NewsletterSubscriber
-        fields = ['email']
--- home/models.py
+++ home/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-
-class NewsletterSubscriber(models.Model):
-    email = models.EmailField(unique=True, max_length=100)
-    subscribed_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email
+# Create your models here.
