de55c86cee4a08ec01de6592f725dd038e10387c
Mark Dunne <Markdunne117@gmail.com>
2024-02-18 22:06:15 +0000

    Add contact model

--- contact/models.py
+++ contact/models.py
@@ -7,3 +7,13 @@ class NewsletterSubscriber(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    subject = models.CharField(max_length=254, null=False, blank=False)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.subject
