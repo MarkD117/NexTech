8d6da17e1411cf1eca46210afe96789ce8cc38aa
Mark Dunne <Markdunne117@gmail.com>
2024-02-19 17:15:21 +0000

    Modify user field on wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
-class Wishlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class WishList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"Wishlist for {self.user.username}"
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import WishList
+from products.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def view_wishlist(request):
+    # Retrieve the wishlist of the current user
+    wishlist = WishList.objects.get(user=request.user)
+
+    context = {
+        'wishlist': wishlist
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    product = Product.objects.get(id=product_id)
+    wishlist, created = WishList.objects.get_or_create(user=request.user)
+    wishlist.products.add(product)
+    return redirect('product_detail', product_id=product_id)
+
+
+@login_required
+def remove_from_wishlist(request, product_id):
+    product = Product.objects.get(id=product_id)
+    wishlist = WishList.objects.get(user=request.user)
+    wishlist.products.remove(product)
+    return redirect('product_detail', product_id=product_id)
