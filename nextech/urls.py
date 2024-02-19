e8a40a1957e50639657dc0965ef6cd3f3396bdd9
Mark Dunne <Markdunne117@gmail.com>
2024-02-19 14:09:32 +0000

    Create and migrate wishlist model

--- nextech/urls.py
+++ nextech/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
-    path('wishlist/', include('wishlist.urls')),
+    # path('wishlist/', include('wishlist.urls')),
     # Using static function to add media url
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
