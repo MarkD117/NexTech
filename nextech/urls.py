1f76be126f9c5c341b2f3951b7ce9667c7207da7
Mark Dunne <Markdunne117@gmail.com>
2024-02-20 00:57:36 +0000

    Add custom 404 error page

--- nextech/urls.py
+++ nextech/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     # Using static function to add media url
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'nextech.views.handler404'
