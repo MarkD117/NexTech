56b24ffd47ff5994417da32d1bea5f16bbe839d2
Mark Dunne <Markdunne117@gmail.com>
2024-02-21 01:54:39 +0000

    Add validation modifications for wishlist python files

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
+
 class WishList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
--- wishlist/views.py
+++ wishlist/views.py
@@ -5,16 +5,16 @@ from .models import WishList
 from products.models import Product
 
 
-
 @login_required
 def view_wishlist(request):
+    """ This view gets wishlisted items and sends them to the template. """
     try:
         # Try to retrieve the wishlist of the current user
         wishlist = WishList.objects.get(user=request.user)
     except WishList.DoesNotExist:
         # If wishlist doesn't exist for the user, create a new one
         wishlist = WishList.objects.create(user=request.user)
-    
+
     context = {
         'wishlist': wishlist
     }
@@ -24,6 +24,7 @@ def view_wishlist(request):
 
 @login_required
 def add_to_wishlist(request, product_id):
+    """ This view handles adding products to the wishlist """
     product = Product.objects.get(id=product_id)
     wishlist, created = WishList.objects.get_or_create(user=request.user)
     wishlist.products.add(product)
@@ -33,6 +34,7 @@ def add_to_wishlist(request, product_id):
 
 @login_required
 def remove_from_wishlist(request, product_id):
+    """ This view handles removing products to the wishlist """
     product = Product.objects.get(id=product_id)
     wishlist = WishList.objects.get(user=request.user)
     wishlist.products.remove(product)
